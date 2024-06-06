fe67ef773620a6745f8f563e19f264b1e210f6f2
Johnmark1130 <mayotejohnmark13@gmail.com>
2024-06-06 23:05:08 +0800

    Cupcake

---
+++ app/src/main/java/com/example/cupcake/ui/theme/Color.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF8AC2B5)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF867377)
+val md_theme_light_onPrimaryContainer = Color(0xFF61203F)
+val md_theme_light_secondary = Color(0xFFF5C0D0)
+val md_theme_light_onSecondary = Color(0xFF531919)
+val md_theme_light_secondaryContainer = Color(0xFF6D3E62)
+val md_theme_light_onSecondaryContainer = Color(0xFF3A1F27)
+val md_theme_light_tertiary = Color(0xFF614329)
+val md_theme_light_onTertiary = Color(0xFF928C8C)
+val md_theme_light_tertiaryContainer = Color(0xFF6D5F55)
+val md_theme_light_onTertiaryContainer = Color(0xFF2E1500)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF201A1B)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF201A1B)
+val md_theme_light_surfaceVariant = Color(0xFFF2DDE2)
+val md_theme_light_onSurfaceVariant = Color(0xFF514347)
+val md_theme_light_outline = Color(0xFF837377)
+val md_theme_light_inverseOnSurface = Color(0xFFFAEEEF)
+val md_theme_light_inverseSurface = Color(0xFF352F30)
+val md_theme_light_inversePrimary = Color(0xFFEE8CAB)
+val md_theme_light_surfaceTint = Color(0xFF865367)
+val md_theme_light_outlineVariant = Color(0xFFD5C2C6)
+val md_theme_light_scrim = Color(0xFF272626)
+
+val md_theme_dark_primary = Color(0xFF2E2E2E)
+val md_theme_dark_onPrimary = Color(0xFF5E1133)
+val md_theme_dark_primaryContainer = Color(0xFF7B294A)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFD9E2)
+val md_theme_dark_secondary = Color(0xFFDD9DAE)
+val md_theme_dark_onSecondary = Color(0xFF422931)
+val md_theme_dark_secondaryContainer = Color(0xFF5A3F47)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFD9E2)
+val md_theme_dark_tertiary = Color(0xFFEFBD94)
+val md_theme_dark_onTertiary = Color(0xFF48290C)
+val md_theme_dark_tertiaryContainer = Color(0xFF623F20)
+val md_theme_dark_onTertiaryContainer = Color(0xFF684241)
+val md_theme_dark_error = Color(0xFFFAA095)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF5A4448)
+val md_theme_dark_onBackground = Color(0xFF613B3E)
+val md_theme_dark_surface = Color(0xFF201A1B)
+val md_theme_dark_onSurface = Color(0xFFEBE0E1)
+val md_theme_dark_surfaceVariant = Color(0xFF70575E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD5C2C6)
+val md_theme_dark_outline = Color(0xFF9E8C90)
+val md_theme_dark_inverseOnSurface = Color(0xFF201A1B)
+val md_theme_dark_inverseSurface = Color(0xFF5F4144)
+val md_theme_dark_inversePrimary = Color(0xFF63253D)
+val md_theme_dark_surfaceTint = Color(0xFFFFB0C9)
+val md_theme_dark_outlineVariant = Color(0xFF614A51)
+val md_theme_dark_scrim = Color(0xFF2B2A2A)
